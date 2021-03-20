c747cecb199ab44591ead8bb5bc3b53066001734
C3ZYM9 <reka.szabo5@stud.uni-corvinus.hu>
2021-03-20 13:16:39 +0100

    6.gyak

--- HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -1,22 +1,19 @@
-﻿window.onload = function () {
-let ide = document.getElementById("pascal");
-    for (var sor = 0; sor < 10; sor++)
-    {
+﻿window.onload = mehet()
+{
+    let hova = document.getElementById("ide");
+    hova.innerHTML = "";
+
+    for (var s = 0; s < 10; s++) {
         let sor = document.createElement("div");
-        sor.appendChild("sor");
-        pascal.appendChild("sor");
-    //új div létrehozása az új sornak
-    //új div osztálylistájához add hozzá a "sor"-t
-    //új div-et add hozzá a "pascal" gyermekeihez
-        for (var oszlop = 0; oszlop <= sor; oszlop++)
-        {
-            let elem = document.createElement("div");
-            elem.appendChild("elem");
-            sor.appendChild("elem");
-            //új div létrehozása az új elemnek
-            //új elem div osztálylistájához add hozzá az "elem"-et
-            //teszteléshet .innerHTML = `${sor}:${oszlop}`
-            //új elem div-et vedd fel a sor elemei közé
+        hova.appendChild(sor);
+        sor.classList.add("egymás_mellé");
+
+        for (var o = 0; o < 10; o++) {
+            let szám = document.createElement("div");
+            sor.appendChild(szám);
+            szám.innerText = (s + 1) * (o + 1);
+            szám.classList.add("doboz");
+            szám.style.color = `rgb(${255 / 10 * s},0,${255 / 10 * o})`;
         }
     }
 }
