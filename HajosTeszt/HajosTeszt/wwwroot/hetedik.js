09639b4fe99c4b1d88b182add0e33612cd0a0323
C3ZYM9 <reka.szabo5@stud.uni-corvinus.hu>
2021-03-26 18:06:37 +0100

    7.gyak

---
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -0,0 +1,47 @@
+﻿var kérdések;
+var kérdésSzám = 0;
+
+function letöltés() {
+    fetch('/questions.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data));
+}
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    kérdések = d;
+    KérdésMegjelenítés(0);
+}
+
+var kérdésmegjelenítés = function (kérdésSzám) {
+    let kérdés_szöveg = document.getElementById("kérdés_szöveg");
+    let kép = document.getElementById("kép1");
+    let válasz1 = document.getElementById("válasz1");
+    let válasz2 = document.getElementById("válasz2");
+    let válasz3 = document.getElementById("válasz3");
+    let válasz4 = document.getElementById("válasz4");
+
+    kérdés_szöveg.innerHTML = kérdések[kérdésSzáma].questionText;
+    kép.src = "https://szoft1.comeback.hu/hajo/" + kérdések[kérdésSzáma].image;
+    válasz1.innerText = kérdések[kérdésSzáma].answer1;
+    válasz2.innerText = kérdések[kérdésSzáma].answer2;
+    válasz3.innerText = kérdések[kérdésSzáma].answer3;
+}
+
+window.onload = () => {
+    letöltés();
+
+    document.getElementById("vissza").onclick = () => {
+        kérdésSzám--;
+        if (kérdésSzám == -1) {
+            kérdésmegjelenítés(kérdésSzám[2]);
+        }
+    }
+
+    document.getElementById("előre").onclick = () => {
+        if (kérdésSzám == 3) {
+            kérdésmegjelenítés(kérdésSzám[0]);
+        }
+    }
+}
