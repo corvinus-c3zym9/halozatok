f494c84013ba839c830ee20c72b46b1f3ae94ab2
C3ZYM9 <reka.szabo5@stud.uni-corvinus.hu>
2021-04-16 11:14:02 +0200

    8.gyak

--- HajosTeszt/HajosTeszt/Startup.cs
+++ HajosTeszt/HajosTeszt/Startup.cs
@@ -16,6 +16,7 @@ namespace HajosTeszt
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,21 +28,31 @@ namespace HajosTeszt
             }
 
             app.UseHttpsRedirection();
-            DefaultFilesOptions options = new DefaultFilesOptions();
-            options.DefaultFileNames.Clear();
-            options.DefaultFileNames.Add("hajo.html");
-            app.UseDefaultFiles(options);
+
+            app.UseDefaultFiles();
+
+            //DefaultFilesOptions options = new DefaultFilesOptions();
+            //options.DefaultFileNames.Clear();
+            //options.DefaultFileNames.Add("hajo.html");
+            //app.UseDefaultFiles(options);
+
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            //app.UseEndpoints(endpoints =>
+            //{
+            //endpoints.MapGet("/", async context =>
+            //{
+            //await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
+
         }
     }
 }
